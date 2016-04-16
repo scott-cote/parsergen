baf097bad4dc495edf609e074e085d5684bf8bde
Scott Cote <cote.scott.f@gmail.com>
2016-04-16 15:32:55 -0400

    Added build environment

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var rename = require('gulp-rename');
 var insert = require('gulp-insert');
+var chmod = require('gulp-chmod');
 var merge = require('merge-stream');
 
 gulp.task('scripts', function() {
@@ -9,10 +10,11 @@ gulp.task('scripts', function() {
     .pipe(babel())
     .pipe(rename(function (path) { path.extname = ".js"; }))
     .pipe(gulp.dest('bin'));
-  var cli = gulp.src('cli.es6')
+  var cli = gulp.src(['cli.es6','test.es6'])
     .pipe(babel())
     .pipe(rename(function (path) { path.extname = ".js"; }))
     .pipe(insert.prepend('#!/usr/bin/env node\n\n'))
+    .pipe(chmod(755))
     .pipe(gulp.dest('bin'));
   return merge(src, cli);
 });
