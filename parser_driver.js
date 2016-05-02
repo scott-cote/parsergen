e7cf2b41ae9564bd5a9c28bb8b445b40afcfae4d
Scott Cote <cote.scott.f@gmail.com>
2016-05-01 21:46:32 -0400

    Checkpoint

--- parser_driver.js
+++ parser_driver.js
@@ -1,25 +1,3 @@
-/*
-var fs = require('fs');
-var thru = require('through2');
-var Parser = require('./parser.js').default;
-var scanner = require('./bin/scanner.js').default;
-var compile = require('./bin/compile.js').default;
-
-var parser = new Parser();
-
-var reader = fs.createReadStream('simple.grammar');
-*/
-/*
-reader.on('end', function() {
-  var nodes = parser.end();
-  var rules = compile(nodes);
-  console.log(JSON.stringify(rules))
-});
-
-reader.pipe(scanner(function(token) {
-  parser.processToken(token.content, token.type);
-}));
-*/
 
 var fs = require('fs');
 var through2 = require('through2')
@@ -43,8 +21,3 @@ var comp = through2.obj(function(chunk, encoding, callback) {
 var reader = fs.createReadStream('simple.grammar');
 
 reader.pipe(scanner()).pipe(par).pipe(comp).pipe(process.stdout)
-
-
-
-//objectStream.write({ status: 404, message: 'Not found' })
-//objectStream.write({ status: 500, message: 'Internal server error'})
