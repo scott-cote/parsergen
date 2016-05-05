fce5635e3f1044f9933e92d44d2f55b245b7d965
Scott Cote <cote.scott.f@gmail.com>
2016-05-04 21:29:57 -0400

    Checkpoint

--- bin/compiler.js
+++ bin/compiler.js
@@ -6,44 +6,56 @@ Object.defineProperty(exports, "__esModule", {
 var through2 = require('through2');
 
 var compiler = function compiler() {
-  /*
-  let getRulesRoot = function() {
-    let root = nodes[nodes.length-1];
-    return nodes[root.children[0]];
+
+  var compile = function compile(nodes) {
+
+    var getRulesRoot = function getRulesRoot() {
+      var root = nodes[nodes.length - 1];
+      return nodes[root.children[0]];
+    };
+
+    var compileLeft = function compileLeft(current) {
+      var ident = nodes[current.children[0]];
+      return ident.contents;
+    };
+
+    var compileRight = function compileRight(current) {
+      if (current.type === 'RIGHT') {
+        return current.children.map(function (id) {
+          return compileRight(nodes[id]);
+        }).reduce(function (value, elements) {
+          return value.concat(elements);
+        }, []);
+      } else {
+        return current.contents;
+      }
+    };
+
+    var compileRule = function compileRule(current) {
+      var left = nodes[current.children[0]];
+      var right = nodes[current.children[2]];
+      return { left: compileLeft(left), right: compileRight(right) };
+    };
+
+    var compileRules = function compileRules(current) {
+      if (current.type === 'RULES') {
+        return current.children.map(function (id) {
+          return compileRules(nodes[id]);
+        }).reduce(function (value, rules) {
+          return value.concat(rules);
+        }, []);
+      } else {
+        return compileRule(current);
+      }
+    };
+
+    var root = getRulesRoot();
+
+    return compileRules(root);
   };
-   let compileLeft = function(current) {
-    let ident = nodes[current.children[0]];
-    return ident.contents;
-  };
-   let compileRight = function(current) {
-    if (current.type === 'RIGHT') {
-      return current.children
-        .map(id => compileRight(nodes[id]))
-        .reduce((value, elements) => value.concat(elements), []);
-    } else {
-      return current.contents;
-    }
-  };
-   let compileRule = function(current) {
-    let left = nodes[current.children[0]];
-    let right = nodes[current.children[2]];
-    return { left: compileLeft(left), right: compileRight(right) };
-  };
-   let compileRules = function(current) {
-    if (current.type === 'RULES') {
-      return current.children
-        .map(id => compileRules(nodes[id]))
-        .reduce((value, rules) => value.concat(rules), []);
-    } else {
-      return compileRule(current);
-    }
-  };
-   let root = getRulesRoot();
-   return compileRules(root);
-  */
 
   return through2.obj(function (chunk, encoding, callback) {
-    this.push(JSON.stringify(chunk) + '\n');
+    this.push(JSON.stringify(compile(chunk)));
     callback();
   });
 };
--- src/compiler.es6
+++ src/compiler.es6
@@ -1,51 +1,53 @@
 var through2 = require('through2');
 
 let compiler = function() {
-  /*
-  let getRulesRoot = function() {
-    let root = nodes[nodes.length-1];
-    return nodes[root.children[0]];
+
+  let compile = function(nodes) {
+
+    let getRulesRoot = function() {
+      let root = nodes[nodes.length-1];
+      return nodes[root.children[0]];
+    };
+
+    let compileLeft = function(current) {
+      let ident = nodes[current.children[0]];
+      return ident.contents;
+    };
+
+    let compileRight = function(current) {
+      if (current.type === 'RIGHT') {
+        return current.children
+          .map(id => compileRight(nodes[id]))
+          .reduce((value, elements) => value.concat(elements), []);
+      } else {
+        return current.contents;
+      }
+    };
+
+    let compileRule = function(current) {
+      let left = nodes[current.children[0]];
+      let right = nodes[current.children[2]];
+      return { left: compileLeft(left), right: compileRight(right) };
+    };
+
+    let compileRules = function(current) {
+      if (current.type === 'RULES') {
+        return current.children
+          .map(id => compileRules(nodes[id]))
+          .reduce((value, rules) => value.concat(rules), []);
+      } else {
+        return compileRule(current);
+      }
+    };
+
+    let root = getRulesRoot();
+
+    return compileRules(root);
   };
-
-  let compileLeft = function(current) {
-    let ident = nodes[current.children[0]];
-    return ident.contents;
-  };
-
-  let compileRight = function(current) {
-    if (current.type === 'RIGHT') {
-      return current.children
-        .map(id => compileRight(nodes[id]))
-        .reduce((value, elements) => value.concat(elements), []);
-    } else {
-      return current.contents;
-    }
-  };
-
-  let compileRule = function(current) {
-    let left = nodes[current.children[0]];
-    let right = nodes[current.children[2]];
-    return { left: compileLeft(left), right: compileRight(right) };
-  };
-
-  let compileRules = function(current) {
-    if (current.type === 'RULES') {
-      return current.children
-        .map(id => compileRules(nodes[id]))
-        .reduce((value, rules) => value.concat(rules), []);
-    } else {
-      return compileRule(current);
-    }
-  };
-
-  let root = getRulesRoot();
-
-  return compileRules(root);
-  */
 
   return through2.obj(function(chunk, encoding, callback) {
-      this.push(JSON.stringify(chunk)+'\n')
-      callback()
+      this.push(JSON.stringify(compile(chunk)));
+      callback();
   })
 };
 
