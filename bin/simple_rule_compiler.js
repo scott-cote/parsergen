c8d8336400eeba894a6de68826392a6f59d271b3
Scott Cote <cote.scott.f@gmail.com>
2016-05-15 12:23:44 -0400

    Checkpoint

--- bin/simple_rule_compiler.js
+++ bin/simple_rule_compiler.js
@@ -26,15 +26,15 @@ var compiler = function compiler() {
         right: [code.complexRules[0].left, '$']
       });
 
-      code.nonterminals = new Set(code.complexRules.map(function (rule) {
+      code.nonterminals = new Set(code.rules.map(function (rule) {
         return rule.left;
       }));
 
-      code.terminals = new Set(code.complexRules.map(function (rule) {
+      code.terminals = new Set(code.rules.map(function (rule) {
         return rule.right;
       }).reduce(function (value, symbols) {
         return value.concat(symbols);
-      }, ['$']).filter(function (symbol) {
+      }, []).filter(function (symbol) {
         return !code.nonterminals.has(symbol);
       }));
 
--- src/simple_rule_compiler.es6
+++ src/simple_rule_compiler.es6
@@ -13,12 +13,12 @@ let compiler = function() {
       });
 
       code.nonterminals = new Set(
-        code.complexRules.map(rule => rule.left)
+        code.rules.map(rule => rule.left)
       );
 
       code.terminals = new Set(
-        code.complexRules.map(rule => rule.right)
-          .reduce((value, symbols) => value.concat(symbols), ['$'])
+        code.rules.map(rule => rule.right)
+          .reduce((value, symbols) => value.concat(symbols), [])
           .filter(symbol => !code.nonterminals.has(symbol))
       );
 
