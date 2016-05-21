21a65eea8ef4bc60e472a712620a48b2b1343b3c
Scott Cote <cote.scott.f@gmail.com>
2016-05-21 12:16:04 -0400

    Checkpoint

--- bin/first_table_compiler.js
+++ bin/first_table_compiler.js
@@ -11,7 +11,62 @@ var _through2 = _interopRequireDefault(_through);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var compiler = function compiler() {
+
+  var compile = function compile(code) {
+
+    console.log(JSON.stringify(code.symbols));
+
+    code.firstTable = code.symbols.reduce(function (firstTable, symbol) {
+
+      var getRulesFor = function getRulesFor(symbol) {
+        return code.rules.filter(function (rule) {
+          return rule.left === symbol;
+        });
+      };
+
+      var getFirstForRule = function getFirstForRule(rule) {
+        return rule.right.reduce(function (cntx, element) {
+          if (!cntx.done) {
+            var first = getFirstSetFor(element.symbol);
+            if (!first.canBeEmpty) {
+              cntx.done = true;
+              cntx.canBeEmpty = false;
+            }
+            cntx.symbols = cntx.symbols.concat(first);
+          }
+          return cntx;
+        }, { done: false, canBeEmpty: true, symbols: [] });
+      };
+
+      var getFirstSetFor = function getFirstSetFor(symbol) {
+        var result = { canBeEmpty: false, symbols: [] };
+
+        if (code.terminals.has(symbol)) {
+          result.symbols.push(symbol);
+        } else {
+          getRulesFor(symbol).forEach(function (rule) {
+            if (rule.right.length === 0) {
+              result.canBeEmpty = true;
+            } else {
+              var first = getFirstForRule(rule);
+              if (first.canBeEmpty) result.canBeEmpty = true;
+              result.symbols = result.symbols.concat(first.symbols);
+            }
+          });
+        }
+
+        return firstTable[symbol] = result;
+      };
+
+      cntx[symbol] = getFirstSetFor(symbol);
+      return firstTable;
+    }, {});
+
+    return code;
+  };
+
   return _through2.default.obj(function (code, encoding, done) {
+    //this.push(compile(code));
     this.push(code);
     done();
   });
--- src/first_table_compiler.es6
+++ src/first_table_compiler.es6
@@ -1,7 +1,60 @@
 import thru from 'through2';
 
 let compiler = function() {
+
+  let compile = function(code) {
+
+    console.log(JSON.stringify(code.symbols))
+
+    code.firstTable = code.symbols.reduce((firstTable, symbol) => {
+
+      let getRulesFor = function(symbol) {
+        return code.rules.filter(rule => rule.left === symbol);
+      };
+
+      let getFirstForRule = function(rule) {
+        return rule.right.reduce((cntx, element) => {
+          if (!cntx.done) {
+            let first = getFirstSetFor(element.symbol);
+            if (!first.canBeEmpty) {
+              cntx.done = true;
+              cntx.canBeEmpty = false;
+            }
+            cntx.symbols = cntx.symbols.concat(first);
+          }
+          return cntx;
+        }, { done: false, canBeEmpty: true, symbols: [] });
+      };
+
+      let getFirstSetFor = function(symbol) {
+        let result = { canBeEmpty: false, symbols: [] };
+
+        if (code.terminals.has(symbol)) {
+          result.symbols.push(symbol);
+        } else {
+          getRulesFor(symbol).forEach(rule => {
+            if (rule.right.length === 0) {
+              result.canBeEmpty = true;
+            } else {
+              let first = getFirstForRule(rule);
+              if (first.canBeEmpty) result.canBeEmpty = true;
+              result.symbols = result.symbols.concat(first.symbols);
+            }
+          });
+        }
+
+        return firstTable[symbol] = result;
+      };
+
+      cntx[symbol] = getFirstSetFor(symbol);
+      return firstTable;
+    }, {});
+
+    return code;
+  };
+
   return thru.obj(function(code, encoding, done) {
+    //this.push(compile(code));
     this.push(code);
     done();
   });
