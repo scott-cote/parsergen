a2c1b1a72333a7513083a58ded0c244defb99060
Scott Cote <cote.scott.f@gmail.com>
2016-06-05 17:03:10 -0400

    Checkpoint

--- bin/first_table_compiler.js
+++ bin/first_table_compiler.js
@@ -10,70 +10,73 @@ var _through2 = _interopRequireDefault(_through);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var compiler = function compiler() {
+var compile = function compile(code) {
 
-  var compile = function compile(code) {
+  code.firstTable = Array.from(code.terminals.keys()).reduce(function (table, symbol) {
+    table[symbol] = { canBeEmpty: false, symbols: [symbol] };
+    return table;
+  }, {});
 
-    code.firstTable = Array.from(code.terminals.keys()).reduce(function (table, symbol) {
-      table[symbol] = { canBeEmpty: false, symbols: [symbol] };
-      return table;
-    }, {});
+  var symbols = Array.from(code.nonterminals.keys()).reverse();
 
-    var symbols = Array.from(code.nonterminals.keys()).reverse();
+  while (symbols.length) {
+    symbols = symbols.reduce(function (cntx, symbol) {
+      var rules = code.rules.filter(function (rule) {
+        return rule.left === symbol;
+      });
 
-    while (symbols.length) {
-      symbols = symbols.reduce(function (cntx, symbol) {
-        var rules = code.rules.filter(function (rule) {
-          return rule.left === symbol;
-        });
-
-        var ready = rules.reduce(function (ready, rule) {
+      var ready = rules.reduce(function (ready, rule) {
+        if (!ready) return false;
+        return rule.right.reduce(function (ready, element) {
           if (!ready) return false;
-          return rule.right.reduce(function (ready, element) {
-            if (!ready) return false;
-            if (element.symbol === symbol) return true;
-            return !!code.firstTable[element.symbol];
-          }, true);
+          if (element.symbol === symbol) return true;
+          return !!code.firstTable[element.symbol];
         }, true);
+      }, true);
 
-        if (ready) {
-          code.firstTable[symbol] = rules.reduce(function (cntx, rule) {
-            var index = rule.right.findIndex(function (element) {
-              return !element.canBeEmpty;
+      if (ready) {
+        code.firstTable[symbol] = rules.reduce(function (cntx, rule) {
+          var index = rule.right.findIndex(function (element) {
+            return !element.canBeEmpty;
+          });
+          var symbols = void 0;
+          if (index === -1) {
+            cntx.canBeEmpty = true;
+            symbols = rule.right.map(function (element) {
+              return element.symbol;
             });
-            var symbols = void 0;
-            if (index === -1) {
-              cntx.canBeEmpty = true;
-              symbols = rule.right.map(function (element) {
-                return element.symbol;
-              });
-            } else {
-              symbols = rule.right.slice(0, index + 1).map(function (element) {
-                return element.symbol;
-              });
-            }
-            cntx.symbols = cntx.symbols.concat(symbols.filter(function (current) {
-              return current != symbol;
-            }).map(function (current) {
-              return code.firstTable[current].symbols;
-            }));
-            return cntx;
-          }, { canBeEmpty: false, symbols: [] });
-        } else {
-          cntx.symbols.push(symbol);
-        }
+          } else {
+            symbols = rule.right.slice(0, index + 1).map(function (element) {
+              return element.symbol;
+            });
+          }
+          cntx.symbols = cntx.symbols.concat(symbols.filter(function (current) {
+            return current != symbol;
+          }).map(function (current) {
+            return code.firstTable[current].symbols;
+          }));
+          return cntx;
+        }, { canBeEmpty: false, symbols: [] });
+      } else {
+        cntx.symbols.push(symbol);
+      }
 
-        return cntx;
-      }, { symbols: [], table: code.firstTable }).symbols;
-    }
+      return cntx;
+    }, { symbols: [], table: code.firstTable }).symbols;
+  }
 
-    return code;
-  };
+  return code;
+};
 
+var compiler = function compiler() {
   return _through2.default.obj(function (code, encoding, done) {
     this.push(compile(code));
     done();
   });
+};
+
+compiler.testAPI = {
+  compile: compile
 };
 
 exports.default = compiler;
--- src/first_table_compiler.es6
+++ src/first_table_compiler.es6
@@ -1,61 +1,64 @@
 import thru from 'through2';
 
-let compiler = function() {
+let compile = function(code) {
 
-  let compile = function(code) {
+  code.firstTable = Array.from(code.terminals.keys()).reduce((table, symbol) => {
+    table[symbol] = { canBeEmpty: false, symbols: [symbol] };
+    return table;
+  }, {});
 
-    code.firstTable = Array.from(code.terminals.keys()).reduce((table, symbol) => {
-      table[symbol] = { canBeEmpty: false, symbols: [symbol] };
-      return table;
-    }, {});
+  let symbols = Array.from(code.nonterminals.keys()).reverse();
 
-    let symbols = Array.from(code.nonterminals.keys()).reverse();
+  while (symbols.length) {
+    symbols = symbols.reduce((cntx, symbol) => {
+      let rules =  code.rules.filter(rule => rule.left === symbol);
 
-    while (symbols.length) {
-      symbols = symbols.reduce((cntx, symbol) => {
-        let rules =  code.rules.filter(rule => rule.left === symbol);
-
-        let ready = rules.reduce((ready, rule) => {
+      let ready = rules.reduce((ready, rule) => {
+        if (!ready) return false;
+        return rule.right.reduce((ready, element) => {
           if (!ready) return false;
-          return rule.right.reduce((ready, element) => {
-            if (!ready) return false;
-            if (element.symbol === symbol) return true;
-            return !!code.firstTable[element.symbol];
-          }, true);
+          if (element.symbol === symbol) return true;
+          return !!code.firstTable[element.symbol];
         }, true);
+      }, true);
 
-        if (ready) {
-          code.firstTable[symbol] = rules.reduce((cntx, rule) => {
-            let index = rule.right.findIndex(element => {
-              return !element.canBeEmpty;
-            });
-            let symbols;
-            if (index === -1) {
-              cntx.canBeEmpty = true;
-              symbols = rule.right.map(element => element.symbol);
-            } else {
-              symbols = rule.right.slice(0, index+1).map(element => element.symbol);
-            }
-            cntx.symbols = cntx.symbols.concat(
-              symbols.filter(current => current != symbol).map(current => code.firstTable[current].symbols)
-            );
-            return cntx;
-          }, { canBeEmpty: false, symbols: [] });
-        } else {
-          cntx.symbols.push(symbol);
-        }
+      if (ready) {
+        code.firstTable[symbol] = rules.reduce((cntx, rule) => {
+          let index = rule.right.findIndex(element => {
+            return !element.canBeEmpty;
+          });
+          let symbols;
+          if (index === -1) {
+            cntx.canBeEmpty = true;
+            symbols = rule.right.map(element => element.symbol);
+          } else {
+            symbols = rule.right.slice(0, index+1).map(element => element.symbol);
+          }
+          cntx.symbols = cntx.symbols.concat(
+            symbols.filter(current => current != symbol).map(current => code.firstTable[current].symbols)
+          );
+          return cntx;
+        }, { canBeEmpty: false, symbols: [] });
+      } else {
+        cntx.symbols.push(symbol);
+      }
 
-        return cntx;
-      }, { symbols: [], table: code.firstTable }).symbols;
-    }
+      return cntx;
+    }, { symbols: [], table: code.firstTable }).symbols;
+  }
 
-    return code;
-  };
+  return code;
+};
 
+let compiler = function() {
   return thru.obj(function(code, encoding, done) {
     this.push(compile(code));
     done();
   });
+};
+
+compiler.testAPI = {
+  compile
 };
 
 export default compiler;
