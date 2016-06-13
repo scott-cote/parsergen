85b159d1fb967935051f4a035933372626420909
Scott Cote <cote.scott.f@gmail.com>
2016-06-13 19:33:52 -0400

    Checkpoint

--- test/first_table_compiler.js
+++ test/first_table_compiler.js
@@ -17,12 +17,12 @@ describe('firstTableCompiler', function () {
   describe('generateFirstFor', function () {
 
     /*
-      Rules for First Sets
-      1. If X is a terminal then first(x) is just X
+     Rules for First Sets
+     1. If X is a terminal then first(x) is just X
     2. If there is a production X -> empty set, then add empty set to first(X)
     3. If there is a production X -> Y1 Y2 .. Yk then add first(Y1 Y2 .. Yk) to first(X)
     3a. if Y1 dosen't contain empty set then first(Y1 Y2 .. Yk) equals first(Y1)
-      */
+     */
 
     var terminalRecord = void 0;
     var terminalTable = void 0;
@@ -84,19 +84,19 @@ describe('firstTableCompiler', function () {
   });
 
   /*
-    //  { canBeEmpty: false, symbols: new Set([symbol]) }
-    describe('generateTerminalEntries', () => {
-      it('should return and empty table when passed and empty terminals set', () => {
+   //  { canBeEmpty: false, symbols: new Set([symbol]) }
+   describe('generateTerminalEntries', () => {
+     it('should return and empty table when passed and empty terminals set', () => {
       let terminals = new Set();
       let table = firstTableCompiler.testAPI.generateTerminalEntries(terminals);
       assert.equal(0, Object.keys(table).length);
     });
-      it('should return a table with one key for each terminal', () => {
+     it('should return a table with one key for each terminal', () => {
       let terminals = new Set(['A', 'B', 'C', 'D']);
       let table = firstTableCompiler.testAPI.generateTerminalEntries(terminals);
       assert.lengthOf(Object.keys(table), 4);
     });
-      it('should return a proper table', () => {
+     it('should return a proper table', () => {
       let terminals = new Set(['A', 'B', 'C', 'D']);
       let table = firstTableCompiler.testAPI.generateTerminalEntries(terminals);
       assert.lengthOf(Object.keys(table), 4);
@@ -106,13 +106,13 @@ describe('firstTableCompiler', function () {
       });
     });
   });
-    describe('getDependencies', () => {
-      it('should return an empty set for a rule with a zero length right', () => {
+   describe('getDependencies', () => {
+     it('should return an empty set for a rule with a zero length right', () => {
       let rule = { left: 'A', right: [] };
       let dependencies = firstTableCompiler.testAPI.getDependencies(rule);
       assert.equal(0, dependencies.size);
     });
-      it('should return a set with one element for each dependency', () => {
+     it('should return a set with one element for each dependency', () => {
       let rule = { left: 'A', right: [
         { symbol: 'B' },
         { symbol: 'C' },
@@ -124,7 +124,7 @@ describe('firstTableCompiler', function () {
         assert.equal(true, dependencies.has(symbol));
       });
     });
-      it('should not return the rule itself as a dependency', () => {
+     it('should not return the rule itself as a dependency', () => {
       let rule = { left: 'A', right: [
         { symbol: 'A' },
         { symbol: 'B' },
@@ -140,9 +140,9 @@ describe('firstTableCompiler', function () {
       });
       assert.equal(false, dependencies.has('A'));
     });
-    });
-    describe('augmentRule', () => {
-      it('should agment the rule', () => {
+   });
+   describe('augmentRule', () => {
+     it('should agment the rule', () => {
       let rule = { left: 'A', right: [
         { symbol: 'C' },
         { symbol: 'D' },
@@ -151,9 +151,9 @@ describe('firstTableCompiler', function () {
       let augmentedRule = firstTableCompiler.testAPI.augmentRule(rule);
       assert.deepEqual({ orgRule: rule, dependencies: new Set(['C','D','E']) }, augmentedRule);
     });
-    });
-    describe('compareAugmentedRules', () => {
-      it('should return 1 when ruleA depends on ruleB', () => {
+   });
+   describe('compareAugmentedRules', () => {
+     it('should return 1 when ruleA depends on ruleB', () => {
       let ruleA = firstTableCompiler.testAPI.augmentRule({ left: 'A', right: [
         { symbol: 'C' },
         { symbol: 'D' },
@@ -168,7 +168,7 @@ describe('firstTableCompiler', function () {
       let value = firstTableCompiler.testAPI.compareAugmentedRules(ruleA, ruleB);
       assert.equal(1, value);
     });
-      it('should return 0 when neither rule depends on the other', () => {
+     it('should return 0 when neither rule depends on the other', () => {
       let ruleA = firstTableCompiler.testAPI.augmentRule({ left: 'A', right: [
         { symbol: 'C' },
         { symbol: 'D' },
@@ -182,7 +182,7 @@ describe('firstTableCompiler', function () {
       let value = firstTableCompiler.testAPI.compareAugmentedRules(ruleA, ruleB);
       assert.equal(0, value);
     });
-      it('should return -1 when ruleB depends on ruleA', () => {
+     it('should return -1 when ruleB depends on ruleA', () => {
       let ruleA = firstTableCompiler.testAPI.augmentRule({ left: 'A', right: [
         { symbol: 'C' },
         { symbol: 'D' },
@@ -197,9 +197,9 @@ describe('firstTableCompiler', function () {
       let value = firstTableCompiler.testAPI.compareAugmentedRules(ruleA, ruleB);
       assert.equal(-1, value);
     });
-    });
-    describe('getSortedRules', () => {
-      it('should get the sorted rules', () => {
+   });
+   describe('getSortedRules', () => {
+     it('should get the sorted rules', () => {
       let ruleA = { left: 'A', right: [
         { symbol: 'E' },
         { symbol: 'F' },
@@ -224,6 +224,6 @@ describe('firstTableCompiler', function () {
       let rules = firstTableCompiler.testAPI.getSortedRules([ruleA, ruleB, ruleC, ruleD1, ruleD2]);
       assert.deepEqual(['C','B','A','D','D'], rules.map(rule => rule.left));
     });
-    });
+   });
   */
 });
